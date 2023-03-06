e22a30b163f9acd3639ae608c39f50584becdc48
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-03-06 09:52:27 -0800

    Handle ValueError that occurs when user enters a nonexistent hotel ID

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ if __name__ == "__main__":
     print(df)
     search_id = input("Enter hotel id: ")
     hotel = Hotel(search_id)
-    if hotel.available():
-        hotel.book_reservation()
-        user_name = input("Enter your name: ")
-        reservation_ticket = ReservationTicket(user_name, hotel)
-        print(reservation_ticket.generate_ticket())
-    else:
-        print("Sorry, that hotel is unavailable.")
+
+    try:
+        if hotel.available():
+            hotel.book_reservation()
+            user_name = input("Enter your name: ")
+            reservation_ticket = ReservationTicket(user_name, hotel)
+            print(reservation_ticket.generate_ticket())
+        else:
+            print("Sorry, that hotel is unavailable.")
+    except ValueError:
+        print("Hotel ID does not exist, please try again")
